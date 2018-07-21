7721b5c1b882ac134a90ac5bcac93af52fb45e5e
Joe Justesen <jcjustesen@gmail.com>
2018-07-21 16:53:36 -0400

    Removed webpack, using straight babel.
    Webpack was not really giving me anything besides running
    babel and watching for changes.  I can do that directly with
    babel-watch and remove the complexity.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World!\nAnd another change.\n'));
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
